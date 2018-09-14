f1184d8738bbf859080d5a81285cb5f46d61f9cf
safiur rehman <safiuremailid@gmail.com>
2018-09-14 12:33:08 -0400

    changes in HelloWorld aaa dddd

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -5,7 +5,7 @@ import org.joda.time.LocalTime;
 public class HelloWorld {
 	public static void main(String[] args) {
 		LocalTime currentTime = new LocalTime();
-		System.out.println("Pramod vishkarma and '"""######$1$1$1$$"'''''safiur rehman khan again again and Again and Again My develop Machine current local time is: " + currentTime);
+		System.out.println("Pramod vishkarma and abdul safiur rehman khan again again and Again and Again My develop Machine current local time is: " + currentTime);
 		Greeter greeter = new Greeter();
 		System.out.println(greeter.sayHello());
 	}
