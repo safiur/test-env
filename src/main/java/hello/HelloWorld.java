ed6ba0323fc9f229df8c21134ced979efdae640a
safiur rehman <safiuremailid@gmail.com>
2018-09-14 15:09:58 -0400

    src/main/java/hello/HelloWorld.java safiur rehman abdul hameed

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -5,7 +5,7 @@ import org.joda.time.LocalTime;
 public class HelloWorld {
 	public static void main(String[] args) {
 		LocalTime currentTime = new LocalTime();
-		System.out.println("Safiur rehman and Abdul The current local time is: " + currentTime);
+		System.out.println("Safiur rehman and Abdul hameed The current local time is: " + currentTime);
 		Greeter greeter = new Greeter();
 		System.out.println(greeter.sayHello());
 	}
